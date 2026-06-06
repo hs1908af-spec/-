d7725502e3cbb4ad1fd0118e359d328686b66a27
hs1908af-spec <hs1908af@gmail.com>
2026-06-06 12:01:24 +0900

    Update AppDelegate.swift

--- schedule/AppDelegate.swift
+++ schedule/AppDelegate.swift
@@ -7,28 +7,25 @@
 
 import UIKit
 
-@main
+@main//시스템이 앱을 실행할 때 가장 먼저 찾아오는 시작점.
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // '스케줄' 앱이 켜질 때 사용자가 저장해 둔 과거의 계획 데이터를 안전하게 불러올 준비를 합니다.
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    // 화면(Scene) 연결 상태 관리 단계
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+       // 메인 화면인 'Default Configuration'(SceneDelegate)을 연결하여 사용자가 달력을 볼 수 있게 합니다.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+ /// 사용자가 멀티태스킹 창에서 '스케줄' 앱의 화면을 위로 쓸어올려 완전히 닫았을 때(종료) 호출됩니다.
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        // 사용자가 앱 화면을 완전히 종료했을 때 메모리를 해제하는 역할을 합니다. '스케줄' 앱이 불필요한 기기 메모리를 차지하지 않도록 최적화하는 역할
     }
 
 
